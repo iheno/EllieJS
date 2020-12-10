165de055d0422a3b558bc8cd4b4aed1677cc51c1
iheno <henorodel@gmail.com>
2020-12-10 19:57:30 +0900

    mkdir array

--- lesson/object/object.js
+++ lesson/object/object.js
@@ -125,5 +125,5 @@ console.log(user5);
 const fruit1 = { color: 'red' };
 const fruit2 = { color: 'blue', size: 'big' };
 const mixed = Object.assign({}, fruit1, fruit2);
-console.log(mixed.color);
-console.log(mixed.size);
+console.log(mixed.color); // blue
+console.log(mixed.size); // big
